a324b8c82e113e78fe1a3e94880173eae304aeaf
kostya <konstantin2113@mail.ru>
2024-01-23 19:21:12 +0300

    add post tests

--- simple_form/tests.py
+++ simple_form/tests.py
@@ -15,9 +15,30 @@ class PageInterfaceTest(TestCase):
         response = self.client.get(address)
         self.assertContains(response, text="Your name", status_code=200)
 
-    # def test_post_template_form(self):
-    #     address = shortcuts.reverse("simple_form:name_form")
-    #     post_data = {}
-    #     response = self.client.post(address)
-    #     self.assertContains(response, text="Your name", status_code=200)
+    def test_get_class_form(self):
+        address = shortcuts.reverse("simple_form:class_name_form")
+        response = self.client.get(address)
+        self.assertContains(response, text="Your name", status_code=200)
+
+    def test_name_form_result(self):
+        name = "Pole"
+        address = shortcuts.reverse("simple_form:form_result", args=(name, ))
+        response = self.client.get(address)
+        self.assertContains(response, text=name, status_code=200)
+
+    def test_post_template_form(self):
+        address = shortcuts.reverse("simple_form:name_form")
+        name = "Galya"
+        post_data = {"your_name": name}
+        response = self.client.post(address, data=post_data)
+        correct_redirect_address = shortcuts.reverse("simple_form:form_result", args=(name, ))
+        self.assertRedirects(response, correct_redirect_address)
+
+    def test_post_class_form(self):
+        address = shortcuts.reverse("simple_form:class_name_form")
+        name = "Natasha"
+        post_data = {"your_name": name}
+        response = self.client.post(address, data=post_data)
+        correct_redirect_address = shortcuts.reverse("simple_form:form_result", args=(name, ))
+        self.assertRedirects(response, correct_redirect_address)
 
--- simple_form/urls.py
+++ simple_form/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("", views.list_of_forms, name="form_list"),
     path("template_form/", views.template_name_form, name="name_form"),
     path("class_form/", views.class_name_form, name="class_name_form"),
-    path("form_result/", views.name_form_result, name="form_result"),
     path("form_result/<str:name>", views.name_form_result, name="form_result"),
 ]
