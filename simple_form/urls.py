e1c08d2ff6423ecfb7b4576539a98bb602ddab06
kostya <konstantin2113@mail.ru>
2024-01-23 15:22:45 +0300

    rewrite ftemplate and class forms for redirect methods

--- simple_form/urls.py
+++ simple_form/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("template_form/", views.template_name_form, name="name_form"),
     path("class_form/", views.class_name_form, name="class_name_form"),
     path("form_result/", views.name_form_result, name="form_result"),
+    path("form_result/<str:name>", views.name_form_result, name="form_result"),
 ]
--- simple_form/views.py
+++ simple_form/views.py
@@ -9,20 +9,34 @@ def list_of_forms(request):
 
 
 def template_name_form(request):
-    return shortcuts.render(request, template_name="simple_form/transformer.html")
+    if request.method != "POST":
+        shortcuts.reverse("simple_form:name_form")
+        return shortcuts.render(request, template_name="simple_form/template_form.html")
 
-
-def name_form_result(request):
-    your_name = request.POST.get("your_name", "hello")
-    template_context = {"name": your_name}
-    return shortcuts.render(request, template_name="simple_form/transformer_processing.html", context=template_context)
+    name = request.POST["your_name"]
+    if name:
+        address = shortcuts.reverse("simple_form:form_result",  args=(name, ))
+        return HttpResponseRedirect(address)
+    else:
+        error_message = "Empty name. Please fill out this field."
+        context = {"error_message": error_message}
+        return shortcuts.render(request, template_name="simple_form/template_form.html", context=context)
 
 
 def class_name_form(request):
-    if request.method == "POST":
-        form = NameForm(request.POST)
-        if form.is_valid():
-            redirect_address = shortcuts.reverse("simple_form:name_form")
-            return HttpResponseRedirect(redirect_address)
-    form = NameForm()
-    return shortcuts.render(request, "simple_form/class_form.html", {"form": form})
+    if request.method != "POST":
+        form = NameForm()
+        return shortcuts.render(request, template_name="simple_form/class_form.html", context={"form": form})
+    form = NameForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data["your_name"]
+        redirect_address = shortcuts.reverse("simple_form:form_result", args=(name, ))
+        return HttpResponseRedirect(redirect_address)
+    else:
+        # this another form, part of it may be filled
+        return shortcuts.render(request, template_name="simple_form/class_form.html", context={"form": form})
+
+
+def name_form_result(request, name):
+    template_context = {"name": name}
+    return shortcuts.render(request, template_name="simple_form/transformer_processing.html", context=template_context)
